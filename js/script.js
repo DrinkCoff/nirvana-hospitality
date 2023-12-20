7b7e09f25f077f95f1fd21b89adb15f070c3f7dd
Rakesh P <feelcoffee@hotmail.com>
2023-12-20 15:38:26 -0500

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 let d = new Date();
 document.body.innerHTML = "<h4>Today's date is " + d + "</h4>"
+
+for(i = 0; i < 10; i++){
+    document.body.innerHTML = "<div class="card">"
+    document.body.innerHTML += "<img src="img_avatar.png" alt="Avatar" style="width:100%">"
+    document.body.innerHTML += "<div class="container">"
+    document.body.innerHTML += "<h4><b>John Doe</b></h4>"
+    document.body.innerHTML += "<p>Architect & Engineer</p>"
+    document.body.innerHTML += "</div>"
+    document.body.innerHTML += "</div>"
+}
